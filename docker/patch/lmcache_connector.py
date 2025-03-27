21b628c41afb3390eea61ec168bae2485866a705
Vinit Desai <vinit@enfabrica.net>
2025-03-27 19:22:16 +0000

    patch for lmcache_connector

--- docker/patch/lmcache_connector.py
+++ docker/patch/lmcache_connector.py
@@ -1,14 +1,16 @@
+# SPDX-License-Identifier: Apache-2.0
 """
-Simple KV Cache Connector for Distributed Machine Learning Inference
+LMCache KV Cache Connector for Distributed Machine Learning Inference
 
 The LMCacheConnector can (1) transfer KV caches between prefill vLLM worker
 (KV cache producer) and decode vLLM worker (KV cache consumer) using LMCache;
-(2) offload and share KV caches. Only (2) is supported for now.
+(2) offload and share KV caches.
 """
 
 from typing import TYPE_CHECKING, List, Tuple, Union
 
 import torch
+
 from vllm.config import VllmConfig
 from vllm.distributed.kv_transfer.kv_connector.base import KVConnectorBase
 from vllm.logger import init_logger
@@ -32,10 +34,12 @@ class LMCacheConnector(KVConnectorBase):
         self.transfer_config = config.kv_transfer_config
         self.vllm_config = config
 
+        from lmcache.experimental.cache_engine import LMCacheEngineBuilder
+        from lmcache.integration.vllm.utils import ENGINE_NAME
         from lmcache.integration.vllm.vllm_adapter import (
             RetrieveStatus, StoreStatus, init_lmcache_engine,
-            lmcache_retrieve_kv, lmcache_should_store, lmcache_store_kv)
-
+            lmcache_retrieve_kv, lmcache_should_retrieve, lmcache_should_store,
+            lmcache_store_kv)
         logger.info("Initializing LMCacheConfig under kv_transfer_config %s",
                     self.transfer_config)
 
@@ -43,12 +47,15 @@ class LMCacheConnector(KVConnectorBase):
         self.engine = init_lmcache_engine(config.model_config,
                                           config.parallel_config,
                                           config.cache_config)
+        self.lmcache_engine_name = ENGINE_NAME
+        self.lmcache_engine_builder = LMCacheEngineBuilder
 
         self.model_config = config.model_config
         self.parallel_config = config.parallel_config
         self.cache_config = config.cache_config
         self.lmcache_retrieve_kv = lmcache_retrieve_kv
         self.lmcache_store_kv = lmcache_store_kv
+        self.lmcache_should_retrieve = lmcache_should_retrieve
         self.lmcache_should_store = lmcache_should_store
         self.store_status = StoreStatus
         self.retrieve_status = RetrieveStatus
@@ -60,16 +67,11 @@ class LMCacheConnector(KVConnectorBase):
     ) -> Tuple[Union[torch.Tensor, IntermediateTensors], bool,
                "ModelInputForGPUWithSamplingMetadata"]:
 
-        # TODO(Jiayi): This shouldn't be none for disagg prefill
-        hidden_or_intermediate_states = None
-
-        # TODO (Jiayi): Only normal prefill is supported for now
-        retrieve_status = [self.retrieve_status.PREFILL]
-
-        model_input, bypass_model_exec = self.lmcache_retrieve_kv(
-            model_executable, model_input, self.cache_config, kv_caches,
-            retrieve_status)
-
+        retrieve_status = self.lmcache_should_retrieve(model_input)
+        model_input, bypass_model_exec, hidden_or_intermediate_states =\
+            self.lmcache_retrieve_kv(
+                model_executable, model_input, self.cache_config, kv_caches,
+                retrieve_status)
         return hidden_or_intermediate_states, bypass_model_exec, model_input
 
     def send_kv_caches_and_hidden_states(
@@ -80,8 +82,7 @@ class LMCacheConnector(KVConnectorBase):
         hidden_or_intermediate_states: Union[torch.Tensor,
                                              IntermediateTensors],
     ) -> None:
-        # TODO (Jiayi): Only normal prefill is supported for now
-        #store_status = [self.store_status.PREFILL] * num_reqs
+
         store_status = self.lmcache_should_store(model_input)
         self.lmcache_store_kv(
             self.model_config,
@@ -94,4 +95,4 @@ class LMCacheConnector(KVConnectorBase):
         )
 
     def close(self):
-        self.engine.close()
+        self.lmcache_engine_builder.destroy(self.lmcache_engine_name)
